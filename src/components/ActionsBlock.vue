e2d209736b89a375f0492bef7db7c61e3739bacb
Nikita Ignatovich <nikita@ignatovich.me>
2021-12-24 02:52:19 +0300

    Actions block new design

--- src/components/ActionsBlock.vue
+++ src/components/ActionsBlock.vue
@@ -1,15 +1,47 @@
 <template>
   <div class="wraper">
-    <div class="centered text-center">
-      <h1>
-        <i class="bi bi-arrow-repeat"></i>
-      </h1>
+    <div v-if="attachmentReady" class="centered text-center">
+      <div :class="{uploading}">
+        <h1><i class="bi bi-arrow-repeat"></i></h1>
+      </div>
+      <button @click="startUploading"
+              :disabled="uploading"
+              class="btn btn-sm btn-primary">
+        <span v-if="!uploading">
+          Upload
+        </span>
+        <span v-else>
+          Uploading...
+        </span>
+      </button>
     </div>
   </div>
 </template>
 
 <script>
+export default {
+  computed: {
+    attachmentReady() {
+      return !!this.$store.getters.attachmentPreview;
+    },
+    uploading() {
+      return this.$store.getters.uploading;
+    },
+  },
+  methods: {
+    startUploading() {
+      this.$store.dispatch('upload');
+    },
+  },
+};
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
+.uploading {
+  animation: linear infinite 1.5s spinny;
+}
+
+@keyframes spinny {
+  to { transform: rotate(360deg); }
+}
 </style>
