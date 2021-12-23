e7ec70e17573e909129ed1ee496753d2e9310d7d
Nikita Ignatovich <nikita@ignatovich.me>
2021-12-24 02:51:51 +0300

    Added uploading and error states, upload action, new getters

--- src/store/index.js
+++ src/store/index.js
@@ -3,20 +3,30 @@ import { createStore } from 'vuex';
 export default createStore({
   state: {
     attachmentPreview: null,
+    uploading: false,
+    error: null,
   },
   mutations: {
     SET_ATTACHMENT_PREVIEW(state, url) {
       state.attachmentPreview = url;
+    },
+    SET_UPLOADING_STATUS(state, status) {
+      state.uploading = status;
     },
   },
   actions: {
     setAttachmentUrl({ commit }, url) {
       commit('SET_ATTACHMENT_PREVIEW', url);
     },
+    upload({ commit }) {
+      commit('SET_UPLOADING_STATUS', true);
+    },
   },
   modules: {
   },
   getters: {
     attachmentPreview: (state) => state.attachmentPreview,
+    uploading: (state) => state.uploading,
+    error: (state) => state.error,
   },
 });
