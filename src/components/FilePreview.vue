03a482b53aafcf36e853c05f88b8b4bbeb771a4d
Nikita Ignatovich <nikita@ignatovich.me>
2021-12-24 01:25:33 +0300

    Styling result preview block

--- src/components/FilePreview.vue
+++ src/components/FilePreview.vue
@@ -1,16 +1,21 @@
 <template>
-  <div>
-    <img :src="previewUrl"
-         class="img-fluid" />
+  <div class="wraper p-1">
+    <div class="centered text-center text-secondary">
+      <h1><i class="bi bi-easel-fill"></i></h1>
+      <p>Result will be shown after uploading and processing file</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  computed: {
-    previewUrl() {
-      return this.$store.getters.attachmentPreview;
-    },
-  },
 };
 </script>
+
+<style scoped lang="scss">
+  .wraper {
+    position: relative;
+    height: 100%;
+    width: 100%;
+  }
+</style>
