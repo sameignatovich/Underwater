a50d3c975a0b610e0be6bc87b0beeefed4a19206
Nikita Ignatovich <nikita@ignatovich.me>
2021-12-23 23:38:42 +0300

    Added Vuex

--- src/main.js
+++ src/main.js
@@ -5,5 +5,8 @@ import './registerServiceWorker';
 // Plugins and CSS frameworks
 import '@/assets/js/app';
 import '@/assets/styles/app.scss';
+import store from './store';
 
-createApp(App).mount('#app');
+createApp(App)
+  .use(store)
+  .mount('#app');
