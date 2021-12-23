e09b3434caada0c6ee0c3eddec256b627886ea32
Nikita Ignatovich <nikita@ignatovich.me>
2021-12-24 00:53:34 +0300

    Preview attachment in same block

--- src/components/FileInput.vue
+++ src/components/FileInput.vue
@@ -1,15 +1,22 @@
 <template>
-  <form>
-    <label for="formFileLg" class="form-label">Large file input example</label>
-    <input class="form-control form-control-lg"
+  <div class="form-wraper radius">
+    <img v-if="previewUrl"
+         :src="previewUrl"
+         class="img-fluid attachment-preview fill radius" />
+    <input class="attachment-input fill"
            id="formFileLg"
            type="file"
            @change="setPreviewUrl">
-  </form>
+  </div>
 </template>
 
 <script>
 export default {
+  computed: {
+    previewUrl() {
+      return this.$store.getters.attachmentPreview;
+    },
+  },
   methods: {
     setPreviewUrl(event) {
       const fileUrl = URL.createObjectURL(event.target.files[0]);
@@ -19,5 +26,31 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
+  .form-wraper {
+    position: relative;
+    height: 100%;
+    width: 100%;
+    border: 2px dashed #dee2e6;
+
+    .attachment-input {
+      opacity: 0;
+      cursor: pointer;
+    }
+  }
+
+  .radius {
+    border-radius: .5rem !important;
+  }
+
+  .fill {
+    position: absolute;
+    top: 0;
+    left: 0;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    outline: none;
+  }
 </style>
