da4918c261f00d0e6a3d8c8f950588b9774c1a73
Nikita Ignatovich <nikita@ignatovich.me>
2021-12-24 00:53:01 +0300

    Fixing child elemtnt height:100% attribute  of RoundedBlock

--- src/components/RoundedBlock.vue
+++ src/components/RoundedBlock.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="p-3 bg-light rounded">
+  <div class="data-block p-3 bg-light rounded">
     <slot></slot>
   </div>
 </template>
+
+<style scoped lang="scss">
+  .data-block {
+    height: 1px;
+    min-height: 350px;
+  }
+</style>
