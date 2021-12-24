708c71eab1c5281f3a7d724110252233936a9bec
Nikita Ignatovich <nikita@ignatovich.me>
2021-12-24 03:19:51 +0300

    Added uploaded state, simulating upload process

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,7 @@ export default createStore({
   state: {
     attachmentPreview: null,
     uploading: false,
+    uploaded: false,
     error: null,
   },
   mutations: {
@@ -13,20 +14,29 @@ export default createStore({
     SET_UPLOADING_STATUS(state, status) {
       state.uploading = status;
     },
+    SET_UPLOADED_STATUS(state, status) {
+      state.uploaded = status;
+    },
   },
   actions: {
     setAttachmentUrl({ commit }, url) {
       commit('SET_ATTACHMENT_PREVIEW', url);
+      if (!url) {
+        commit('SET_UPLOADED_STATUS', false);
+      }
     },
     upload({ commit }) {
       commit('SET_UPLOADING_STATUS', true);
+      setTimeout(() => {
+        commit('SET_UPLOADING_STATUS', false);
+        commit('SET_UPLOADED_STATUS', true);
+      }, 5000);
     },
-  },
-  modules: {
   },
   getters: {
     attachmentPreview: (state) => state.attachmentPreview,
     uploading: (state) => state.uploading,
+    uploaded: (state) => state.uploaded,
     error: (state) => state.error,
   },
 });
